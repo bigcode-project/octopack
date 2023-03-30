328cdc1efe5b0f685d7bfc7a7916346a4d36ea2c
Niklas Muennighoff <n.muennighoff@gmail.com>
2023-03-30 09:50:28 +0200

    Create setup.py

---
+++ evaluation/setup.py
@@ -0,0 +1,5 @@
+# Setup evaluation
+git clone -b parity https://github.com/bigcode-project/bigcode-evaluation-harness
+cd bigcode-evaluation-harness
+pip install -q -r requirements.txt
+accelerate config
