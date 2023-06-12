7648c5278050fa12f0cee5751ddf74a1c6027a09
Muennighoff <n.muennighoff@gmail.com>
2023-06-12 09:44:55 +0200

    Fix

--- evaluation/eval_codet5p16b.sh
+++ evaluation/eval_codet5p16b.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=eval
 #SBATCH --ntasks=1                   # number of MP tasks
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10           # number of cores per tasks
+#SBATCH --cpus-per-task=8           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=20:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --output=%x-%j.out          # output file name
@@ -31,3 +31,93 @@ accelerate launch --config_file config_1a100_fp16.yaml main.py \
 --modeltype seq2seq \
 --max_length_generation 2048 \
 --precision fp16
+
+accelerate launch --config_file config_1a100_fp16.yaml main.py \
+--model codet5p-16b \
+--tasks humaneval-x-bugs-js \
+--do_sample True \
+--temperature 0.2 \
+--n_samples 20 \
+--batch_size 5 \
+--allow_code_execution \
+--save_generations \
+--trust_remote_code \
+--mutate_method prompt \
+--save_generations_path generations_humanevalxbugsjs_codet5p16b_temp02.json \
+--metric_output_path evaluation_humanevalxbugsjs_codet5p16b_temp02.json \
+--modeltype seq2seq \
+--max_length_generation 2048 \
+--precision fp16 \
+--generation_only
+
+accelerate launch --config_file config_1a100_fp16.yaml main.py \
+--model codet5p-16b \
+--tasks humaneval-x-bugs-java \
+--do_sample True \
+--temperature 0.2 \
+--n_samples 20 \
+--batch_size 5 \
+--allow_code_execution \
+--save_generations \
+--trust_remote_code \
+--mutate_method prompt \
+--save_generations_path generations_humanevalxbugsjava_codet5p16b_temp02.json \
+--metric_output_path evaluation_humanevalxbugsjava_codet5p16b_temp02.json \
+--modeltype seq2seq \
+--max_length_generation 2048 \
+--precision fp16 \
+--generation_only
+
+accelerate launch --config_file config_1a100_fp16.yaml main.py \
+--model codet5p-16b \
+--tasks humaneval-x-bugs-cpp \
+--do_sample True \
+--temperature 0.2 \
+--n_samples 20 \
+--batch_size 5 \
+--allow_code_execution \
+--save_generations \
+--trust_remote_code \
+--mutate_method prompt \
+--save_generations_path generations_humanevalxbugscpp_codet5p16b_temp02.json \
+--metric_output_path evaluation_humanevalxbugscpp_codet5p16b_temp02.json \
+--modeltype seq2seq \
+--max_length_generation 2048 \
+--precision fp16 \
+--generation_only
+
+accelerate launch --config_file config_1a100_fp16.yaml main.py \
+--model codet5p-16b \
+--tasks humaneval-x-bugs-go \
+--do_sample True \
+--temperature 0.2 \
+--n_samples 20 \
+--batch_size 5 \
+--allow_code_execution \
+--save_generations \
+--trust_remote_code \
+--mutate_method prompt \
+--save_generations_path generations_humanevalxbugsgo_codet5p16b_temp02.json \
+--metric_output_path evaluation_humanevalxbugsgo_codet5p16b_temp02.json \
+--modeltype seq2seq \
+--max_length_generation 2048 \
+--precision fp16 \
+--generation_only
+
+accelerate launch --config_file config_1a100_fp16.yaml main.py \
+--model codet5p-16b \
+--tasks humaneval-x-bugs-rust \
+--do_sample True \
+--temperature 0.2 \
+--n_samples 20 \
+--batch_size 5 \
+--allow_code_execution \
+--save_generations \
+--trust_remote_code \
+--mutate_method prompt \
+--save_generations_path generations_humanevalxbugsrust_codet5p16b_temp02.json \
+--metric_output_path evaluation_humanevalxbugsrust_codet5p16b_temp02.json \
+--modeltype seq2seq \
+--max_length_generation 2048 \
+--precision fp16 \
+--generation_only
--- evaluation/humaneval_x_bugs_setup_java.sh
+++ evaluation/humaneval_x_bugs_setup_java.sh
@@ -1,5 +1,5 @@
 # https://github.com/THUDM/CodeGeeX/blob/61529ba61de8e51c520dc67a3ce4bd62278770df/codegeex/docker/Dockerfile#L31
-!mkdir -p /workspace/download/
-!curl -o /workspace/download/jdk.tar.gz -SL https://download.oracle.com/java/18/archive/jdk-18_linux-x64_bin.tar.gz \
+mkdir -p /workspace/download/
+curl -o /workspace/download/jdk.tar.gz -SL https://download.oracle.com/java/18/archive/jdk-18_linux-x64_bin.tar.gz \
     && mkdir /usr/java && tar -zxf /workspace/download/jdk.tar.gz -C /usr/java && rm /workspace/download/jdk.tar.gz \
     && java_path=`ls /usr/java/${path}` && echo "export JAVA_HOME=/usr/java/${java_path}" >> ~/.profile
