225431372e706d8d51f5a3e743d05b8bdf6c4483
Lambert Pouguem Wassi <lambert.pouguem.wassi@ibm.com>
2024-08-01 10:05:47 -0400

    fixed workdir

--- dataset/commitpack/scrape_github_custom.py
+++ dataset/commitpack/scrape_github_custom.py
@@ -23,6 +23,7 @@ git checkout FETCH_HEAD^ -- README.md
 # In the multiprocessing case, the below leads to each process creating the same directory
 # random = random.Random(42)  # make it reproducible
 
+
 NUM_THREADS = 1
 NUM_PROC = 4
 
@@ -30,7 +31,7 @@ NUM_PROC = 4
 CWD = os.getcwd()
 
 # file path to store the results
-commits_file_path = "dataset/methods2test/ry.jsonl"
+commits_file_path = "dataset/methods2test/methods2test_commits.jsonl"
 
 # file path to store the results
 diffs_file_path = "dataset/methods2test/data/java/methods2test.jsonl"
@@ -39,8 +40,10 @@ diffs_file_path = "dataset/methods2test/data/java/methods2test.jsonl"
 def run_in_shell(cmd: str, cwd=None, timeout=60):
     return subprocess.run([cmd], capture_output=True, shell=True, cwd=cwd, timeout=timeout)
 
-def get_file_contents(commit, old_file, new_file, repo, cwd=None):
-  
+def get_file_contents(commit, old_file, new_file,  repo, random_dir, cwd=None):
+    
+    completed = run_in_shell("git init", cwd=random_dir)
+    completed = run_in_shell("git remote add origin " + repo, cwd=random_dir)
     completed = run_in_shell("git fetch --depth 2 origin " + commit, cwd=cwd)
      # If it requires authentication
     if completed.returncode != 0:
@@ -53,9 +56,11 @@ def get_file_contents(commit, old_file, new_file, repo, cwd=None):
     completed = run_in_shell("git checkout FETCH_HEAD^ -- " + old_file, cwd=cwd)
     # If there's only a new file, but no old file
     if completed.returncode != 0:
+        #print("ERRORC2", completed)
         return (new_contents, "", completed.returncode, completed.stderr.decode(errors='ignore'))
     old_contents = run_in_shell("cat " + old_file, cwd=cwd).stdout.decode(errors='ignore')
     return (new_contents, old_contents, completed.returncode, completed.stderr.decode(errors='ignore'))
+   
 
 def get_commit_diff(ex):
    
@@ -74,7 +79,6 @@ def get_commit_diff(ex):
             completed = run_in_shell("git clone " + repo, cwd=random_dir)
 
             if completed.returncode != 0:
-                
                 logging.error(f'ERROR: Could not clone {repo}')
                 logging.info(f'removing {random_dir}')
                 run_in_shell("rm -rf " + random_dir + "/" + repo.split("/")[-1])
@@ -87,22 +91,21 @@ def get_commit_diff(ex):
 
             if completed.returncode != 0:
                 print(f'ERRORC2: {completed}')
-            
-            for i , commit_id in enumerate(commits[1:4]):
-                if i == len(commits[1:4]) - 1:
-                    ex['is_last_commit'] = True
-                    logging.info(f'last commit reached: Removing {random_dir}')
-                    run_in_shell("rm -rf " + random_dir)
-                ex['commit'] = commit_id
-                file.write(json.dumps(ex) + "\n")
+
+            if len (commits) <  50:
+                for i , commit_id in enumerate(commits):
+                    if i == len(commits) - 1:
+                        ex['is_last_commit'] = True
+                        logging.info(f'last commit reached: Removing {random_dir}')
+                        run_in_shell("rm -rf " + random_dir)
+                    ex['commit'] = commit_id
+                    file.write(json.dumps(ex) + "\n")
             
         except Exception as e:
             
             logging.error(f'ERROR: {e}')
             logging.info(f'removing {random_dir}')
             run_in_shell("rm -rf " + random_dir)
-    
-        
     return ex
 
 
@@ -113,51 +116,61 @@ def get_diff(ex):
     repo_name = repo.split("/")[-1]
     repo_exists = False
     working_dir = ""
-    for path in Path(CWD).rglob('*'):
+
+    for path in Path(CWD).rglob('*'): # get all the repos
         if path.is_dir() and path.name == repo_name:
             print(f'repo exists: {path}')
+            print(f'path name: {path.name}')
             repo_exists = True
+            random_dir = path.parent
             working_dir = path
             break
+   
     with open('dataset/methods2test/methods2test.jsonl', 'a') as methods2test_file:  # Open the file in append mode
+
+        if not repo_exists:
+            random_dir = CWD + "/" + str(random.randint(0, 1000000))
+            run_in_shell("mkdir " + random_dir, timeout=300)
+            completed = run_in_shell("git init", cwd=random_dir)
+            completed = run_in_shell("git remote add origin " + repo, cwd=random_dir)
+            completed = run_in_shell("git clone " + repo, cwd=random_dir)
+            working_dir = random_dir + "/" + repo.split("/")[-1]
+        commit_id = ex['commit']
+
         try:
             # Create a random directory to store the repo
             #check if the ramdom directory exists
-            
-            if not repo_exists:
-                random_dir = CWD + "/" + str(random.randint(0, 1000000))
-                run_in_shell("mkdir " + random_dir, timeout=300)
-                completed = run_in_shell("git init", cwd=random_dir)
-                completed = run_in_shell("git remote add origin " + repo, cwd=random_dir)
-                completed = run_in_shell("git clone " + repo, cwd=random_dir)
-                working_dir = random_dir + "/" + repo.split("/")[-1]
-        
-            
-            commit_id = ex['commit']
-            #get files modified in this commit
-            print(f'getting all files at ' + str(working_dir) + "/" + repo.split("/")[-1] +"for commit " + commit_id + "...\n")
+            logging.info(f'Getting all files at ' + str(working_dir)  + "for commit " + commit_id + "...\n")
             completed = run_in_shell("git diff-tree --no-commit-id --name-only -r " + f'{commit_id}', cwd=working_dir)
-            if completed.returncode != 0:
-                logging.error(f'Could not get files for commit {commit_id}')
-
-            
-            files = completed.stdout.decode(errors='ignore').split("\n") 
-            # show all files
-        
+        except Exception as e:
+            logging.error(f'ERROR: No files found for commit {commit_id} , error: {e}')
            
+            
+        files = completed.stdout.decode(errors='ignore').split("\n") 
+        logging.info(f'files: {files}')
+
+        if completed.returncode != 0:
+            print(f'ERRORC2: {completed}')
+            print(f'no files found for commit {commit_id} , error: {completed.stderr}')
+
+        else:
             for file in files:
+                print(f'file: {file} \n')
                 if file.endswith(".java"):
                     #Assuminng that file names has not changed between commits
+
+                    #check if file exists in the repo
+                    if not os.path.exists(os.path.join(working_dir, file)):
+                        logging.error(f'ERROR: Could not find file {file} in repo {repo}')
+                        continue
+
                     new_file = file
                     old_file = file
-                    logging(f'getting file contents for {new_file}  and  {old_file} at ' + str(working_dir) + "for commit " + commit_id + " for repo " + repo + "...")
-                    
-                    new_contents, old_contents ,_ ,_ = get_file_contents(commit_id, old_file, new_file, repo, cwd=working_dir)
-
+                    new_contents, old_contents ,_ , _ =  get_file_contents(commit_id, old_file, new_file, repo, random_dir,  working_dir)
                     #get commit message
                     completed = run_in_shell("git show --format=%B -s " + commit_id, cwd=working_dir)
                     message = completed.stdout.decode(errors='ignore')
-                    print(f'message: {message} \n')
+                    logging.info(f'message: {message} \n')
                     ex["new_contents"] = new_contents
                     ex["old_contents"] = old_contents
                     ex["new_file"] = new_file
@@ -165,20 +178,15 @@ def get_diff(ex):
                     ex["commit"] =  commit_id
                     ex["message"] = message
                     methods2test_file.write(json.dumps(ex) + "\n")
+                    logging.info(f'ex  written  to file {methods2test_file}')
 
-        except Exception as e:
-            #print("ERROR", commit_id, old_file, new_file, repo, str(random_dir), e)
-            # Break in case of many repos that all lead us nowhere
-         
-            logging.error(f'{e}. Working dir causing this error: {working_dir}')
+    if ex['is_last_commit'] == True:
+        logging.info(f'last commit reached: Removing {working_dir}')
+        logging.info(f'cleaning up {working_dir}')
+        run_in_shell("rm -rf " + str(Path(working_dir)))  # clean up again
+    return ex
+  
         
-        finally:
-            if ex['is_last_commit'] == True:
-                logging.info(f'last commit reached: Removing {working_dir}')
-                logging.info(f'cleaning up {working_dir}')
-                run_in_shell("rm -rf " + str(Path(working_dir)))  # clean up again
-        return ex
-      
 
 def get_commits_multi_threaded_processed(batch):
     with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
@@ -211,13 +219,9 @@ if __name__ == "__main__":
     # # save the dataset
     # ds.to_json("dataset/methods2test/repos_testcases_none.jsonl", num_proc=NUM_PROC)
     
-    cols_to_select = ["commit", "old_file", "new_file", "old_contents", "new_contents", "subject", "message", "lang", "license", "repos"]
-    logging.info(f"Selecting columns: {cols_to_select}")
-    cols_to_select = ["name", "url", "commit", "old_file", "new_file", "old_contents", "new_contents", "subject", "message", "lang", "license", "repos"]
-    ds = ds.select_columns(cols_to_select)
 
-    START = 30 # Modify for each instance (0 - 7)
-    samples_per_instance =  1 * 4 * 1 * 1    # 1 * 4 * 64 * 34 # 8_388_608
+    START = 35 # Modify for each instance (0 - 7)
+    samples_per_instance =  1 * 4 * 5 * 5    # 1 * 4 * 64 * 34 # 8_388_608
     select_start = START * samples_per_instance
     select_end = START * samples_per_instance + samples_per_instance
     ds = ds.select(range(select_start, select_end))
